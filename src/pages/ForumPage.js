46ed1b6da6f1143dde3bed4a4404ed3c35490afa
Vinzeee <88548397+Vinzeee@users.noreply.github.com>
2025-05-20 16:48:01 -0400

    Create ForumPage.js

---
+++ src/pages/ForumPage.js
@@ -0,0 +1,5 @@
+// src/pages/ForumPage.js
+import React, { useState } from 'react';
+import { MagnifyingGlassIcon } from '@heroicons/react/24/outline';
+import StrategyCard from '../components/StrategyCard';
+import { useApp } from '../
